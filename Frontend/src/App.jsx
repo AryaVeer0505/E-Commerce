42d531bccd886515eb89d8e83ee13966cc544d49
AryaVeer0505 <aryaveerkanwar11458@gmail.com>
2025-03-08 22:06:42 +0530

    initial commit

---
+++ Frontend/src/App.jsx
@@ -0,0 +1,33 @@
+import React from 'react'
+import { Routes,Route } from 'react-router-dom'
+import Home from './Pages/Home'
+import Collection from './Pages/Collection'
+import About from './Pages/About'
+import Cart from './Pages/Cart'
+import Contact from './Pages/Contact'
+import Login from './Pages/Login'
+import Orders from './Pages/Orders'
+import PlaceOrder from './Pages/PlaceOrder'
+import Product from './Pages/Product'
+import Navbar from './Components/Navbar'
+
+const App = () => {
+  return (
+    <div>
+        <Navbar/>
+      <Routes>
+        <Route path='/' element={<Home/>}/>
+        <Route path='/collection' element={<Collection/>}/>
+        <Route path='/about' element={<About/>}/>
+        <Route path='/cart' element={<Cart/>}/>
+        <Route path='/contact' element={<Contact/>}/>
+        <Route path='/login' element={<Login/>}/>
+        <Route path='/orders' element={<Orders/>}/>
+        <Route path='/place-order' element={<PlaceOrder/>}/>
+        <Route path='/product/:productId' element={<Product/>}/>
+      </Routes>
+    </div>
+  )
+}
+
+export default App
---
+++ Frontend/src/Components/Navbar.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <div className='navbar'>
+      <div className="logo">
+
+      </div>
+      <div className="nav-links">
+
+      </div>
+      <div className="nav-btns">
+        
+      </div>
+    </div>
+  )
+}
+
+export default Navbar
